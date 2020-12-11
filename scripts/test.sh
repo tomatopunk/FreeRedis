29ccaae6b524e78f477dc6f47685bf3082df2ab6
shaun.zhang <shaun.zhang@ly.com>
2020-12-11 16:15:51 +0800

    update ci

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,13 +5,20 @@ cd test/Unit/ || exit
 for i in *.Tests ; do 
   echo "### Executing Tests for $i:"
   
-  time dotnet test "$i" --no-build \
-    /p:CollectCoverage=true \
-    /p:CoverletOutputFormat="json%2copencover" \
-    /p:CoverletOutput="../results/coverage" \
-    /p:MergeWith="../results/coverage.json" \
-    > /tmp/freeredis-test.log
+##  time dotnet test "$i" --no-build \
+##    /p:CollectCoverage=true \
+#    /p:Exclude="[coverlet.*]*,[*]Coverlet.Core*" \
+##    /p:ExcludeByFile="examples/*"
+##    /p:CoverletOutputFormat="json%2copencover" \
+##    /p:CoverletOutput="../results/coverage" \
+##    /p:MergeWith="../results/coverage.json" \
+##    > /tmp/freeredis-test.log
 
+  time dotnet test "$i" --no-build \
+    --collect:"XPlat Code Coverage" \
+    -- DataCollectionRunSettings.DataCollectors.DataCollector.Configuration.ExcludeByFile="examples/*" \
+    > /tmp/freeredis-test.log
+    
   if [[ $? -ne 0 ]] ; then
     echo "Test Run Failed."
     cat /tmp/freeredis-test.log
