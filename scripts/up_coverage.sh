5deef917353d68f4dbd40e682e7d2dfe8c92fe51
shaun.zhang <shaun.zhang@ly.com>
2020-12-11 14:42:55 +0800

    up

--- scripts/up_coverage.sh
+++ scripts/up_coverage.sh
@@ -2,5 +2,4 @@
 
 curl -s https://codecov.io/bash > codecov
 chmod +x codecov
-./codecov -f "$(Build.SourcesDirectory)/CodeCoverage/Cobertura.xml" -codecov.yml
-t $1
+./codecov -f "$(Build.SourcesDirectory)/CodeCoverage/Cobertura.xml" -t $1
