433138929b3927136dd06d95928b38d41e8c23ed
shaun.zhang <shaun.zhang@ly.com>
2020-12-21 18:58:26 +0800

    更新test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
+# Setup environment variables
+
+docker ps --filter "status="running"" --format "{{.Names}}\t{{.Ports}}\t{{.Image}}" | while read -r name ports image;do
+  key="DOCKER_HOST_$name"
+  # 0.0.0.0:1234->9876/tcp,first
+  tempPort= cut -d'-' -f1 <<< $port
+  echo "$key,$tempPort"
+  export key=ports  
+done
+
+echo "setup is finished"
+
 cd test/Unit/ || exit
 
 for i in *.Tests ; do 
   echo "### Executing Tests for $i:"
   
-##  time dotnet test "$i" --no-build \
-##    /p:CollectCoverage=true \
-#    /p:Exclude="[coverlet.*]*,[*]Coverlet.Core*" \
-##    /p:ExcludeByFile="examples/*"
-##    /p:CoverletOutputFormat="json%2copencover" \
-##    /p:CoverletOutput="../results/coverage" \
-##    /p:MergeWith="../results/coverage.json" \
-##    > /tmp/freeredis-test.log
-
   time dotnet test "$i" --no-build \
     --collect:"XPlat Code Coverage" \
     -- DataCollectionRunSettings.DataCollectors.DataCollector.Configuration.ExcludeByFile="examples/*" \
